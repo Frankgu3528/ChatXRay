73dbc2f44bf87b8d36f72c27bd54ba99cc867f90
katie <3658903965@qq.com>
2024-04-09 05:48:59 -0400

    add generate_json

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
+import PIL.ImageShow
 from datasets import load_from_disk  
-  
+from PIL import Image  
 dataset = load_from_disk("/home/yz979/ggf/ChatXRay/data")
-
-pic = dataset["train"][0]['text']
-print(pic)
+from tqdm import tqdm  
+  
+for i in tqdm(range(len(dataset["train"]))):  
+    pic = dataset["train"][i]['image']
+    # text = dataset["train"][i]['text']
+    # report = dataset["train"][i]['report']
+    pic.save(f"/home/yz979/ggf/ChatXRay/data/pic/image_{i}.jpeg")  
